0d6e6de88aba6f3c3200735523d1c47ab7cdc73e
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-09-23 08:51:52 -0500

    Agregando primer componente MyLabel

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,11 @@
+import './mylabel.css';
+
+const MyLabel = () => {
+  return (
+    <span>
+      my label
+    </span>
+  );
+};
+
+export default MyLabel;
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,12 @@
+import MyLabel from "../../components/MyLabel";
+
+export default {
+  title: 'Components/Label',
+  component: MyLabel
+};
+
+const Template = () => <MyLabel />;
+
+export const Primary = Template.bind({});
+export const Secundary = Template.bind({});
+export const Large = Template.bind({});
